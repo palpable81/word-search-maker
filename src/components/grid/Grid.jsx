2311b64908eb4aca9914f389d71cb841d0b0439e
Vinnie Prabhu <vinnie.prabhu@gmail.com>
2023-02-06 10:59:18 +0700

    added print button

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -6,11 +6,20 @@ import GridRow from './GridRow';
 export default function Grid() {
   const grid = useSelector(selectGrid);
 
+  const handleOnClick = () => {
+    
+  };
+
   return (
-    <div className='grid'>
-      <div className='grid-outline'>
-        {grid.map((gridRow, i) => <GridRow gridRow={gridRow} key={i}/>)}
+    <div>
+      <div className='grid'>
+        <div className='grid-outline'>
+          {grid.map((gridRow, i) => <GridRow gridRow={gridRow} key={i}/>)}
+        </div>
       </div>
+      <button className='print-button' onClick={handleOnClick} >
+        PRINT
+      </button>
     </div>
   );
 }
