9b137c4cf4e421ed9cd0934253e8287eda4ef4f0
Vinnie Prabhu <vinnie.prabhu@gmail.com>
2023-02-09 09:59:54 +0700

    disabled generate button while generating

--- src/components/wordbank/Wordbank.jsx
+++ src/components/wordbank/Wordbank.jsx
@@ -1,52 +1,58 @@
 import './wordbank.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { setWordStatus, selectWordbank } from '../../features/wordbank/wordbankSlice';
-import { placeWord, fillRemainingSquares, clearGrid, selectFinished } from '../../features/grid/gridSlice';
+import { placeWord, fillRemainingSquares, clearGrid, setIsGenerating,
+         selectFinished, selectIsGenerating } from '../../features/grid/gridSlice';
 
 import Word from './Word';
 
 export default function Wordbank() {
   const wordbank = useSelector(selectWordbank);
   const finished = useSelector(selectFinished);
+  const isGenerating = useSelector(selectIsGenerating);
   const dispatch = useDispatch();
 
   const handleOnClick = () => {
-    const sortedWords = wordbank.filter(wordEntry => wordEntry.word
-    ).map((wordEntry, i) => {
-      return {
-        id: i,
-        word: wordEntry.word
+    if(!isGenerating) {
+      dispatch(setIsGenerating(true));
+      const sortedWords = wordbank.filter(wordEntry => wordEntry.word
+        ).map((wordEntry, i) => {
+          return {
+            id: i,
+            word: wordEntry.word
+          }
+        }).sort((a, b) => {
+          return b.word.length - a.word.length;
+        });
+        
+      dispatch(clearGrid());
+  
+      // Returns a Promise that resolves after "ms" Milliseconds
+      const timer = ms => new Promise(res => setTimeout(res, ms))
+  
+      async function fillGrid () { // We need to wrap the loop into an async function for this to work
+        for(let i=0; i < sortedWords.length; i++) {
+          const wordEntry = sortedWords[i];
+  
+          await timer(300);
+          const result = dispatch(placeWord(wordEntry.word.replace(/ /g,'')));
+          dispatch(setWordStatus({
+            id: wordEntry.id,
+            word: wordEntry.word,
+            triedToPlace: true, 
+            placedSuccessfully: result
+          }));
+  
+          if(i === sortedWords.length - 1) {
+            await timer(600);
+            dispatch(fillRemainingSquares());
+            dispatch(setIsGenerating(false));
+          }
+        };
       }
-    }).sort((a, b) => {
-      return b.word.length - a.word.length;
-    });
-    
-    dispatch(clearGrid());
-
-    // Returns a Promise that resolves after "ms" Milliseconds
-    const timer = ms => new Promise(res => setTimeout(res, ms))
-
-    async function fillGrid () { // We need to wrap the loop into an async function for this to work
-      for(let i=0; i < sortedWords.length; i++) {
-        const wordEntry = sortedWords[i];
-
-        await timer(300);
-        const result = dispatch(placeWord(wordEntry.word.replace(/ /g,'')));
-        dispatch(setWordStatus({
-          id: wordEntry.id,
-          word: wordEntry.word,
-          triedToPlace: true, 
-          placedSuccessfully: result
-        }));
-
-        if(i === sortedWords.length - 1) {
-          await timer(600);
-          dispatch(fillRemainingSquares());
-        }
-      };
+  
+      fillGrid();
     }
-
-    fillGrid();
   };
 
   return (
