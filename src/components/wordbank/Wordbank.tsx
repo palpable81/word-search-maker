4cdc24147644462de8a966b98885c648a634e8bb
Vinnie Prabhu <vinnie.prabhu@gmail.com>
2023-05-12 10:46:07 +0700

    add types

--- src/components/wordbank/Wordbank.tsx
+++ src/components/wordbank/Wordbank.tsx
@@ -1,6 +1,6 @@
 import './wordbank.css';
 import { useAppSelector } from '../../app/hooks';
-import { selectWordbank } from '../../features/wordbank/wordbankSlice';
+import { Word, selectWordbank } from '../../features/wordbank/wordbankSlice';
 import WordBox from './WordBox';
 
 export default function Wordbank() {
@@ -8,7 +8,7 @@ export default function Wordbank() {
 
   return (
     <div className='wordbank'>
-      {wordbank.map((wordEntry: any, i: any) => <WordBox wordEntry={wordEntry} key={i} id={i}/>)}
+      {wordbank.map((wordEntry: Word, i: number) => <WordBox wordEntry={wordEntry} key={i} id={i}/>)}
     </div>
   );
 }
