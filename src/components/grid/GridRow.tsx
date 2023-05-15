d1c6f39a084b779c489b88698849532951b2972e
Vinnie Prabhu <vinnie.prabhu@gmail.com>
2023-05-15 11:03:47 +0700

    add type to props

--- src/components/grid/GridRow.tsx
+++ src/components/grid/GridRow.tsx
@@ -5,12 +5,12 @@ interface GridRowProps {
   key: number 
 }
 
-export default function GridRow(props: any) {
+export default function GridRow(props: GridRowProps) {
   const gridRow = props.gridRow;
 
   return (
     <div className='grid-row'>
-        {gridRow.map((char: string, i: number) => (
+        {gridRow.map((char: string | null, i: number) => (
           <div className='grid-box' key={i}>
             <div className={char ? 'grid-letter filled' : 'grid-letter empty'} key={i}>
               {char || ''}
